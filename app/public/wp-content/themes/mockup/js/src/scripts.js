587be5c2105170cda7888dfc7a5e23d8dc26ecb1
Dan <ddkeech@gmail.com>
2024-02-14 11:58:57 -0500

    dotted is, crossed ts

--- app/public/wp-content/themes/mockup/js/src/scripts.js
+++ app/public/wp-content/themes/mockup/js/src/scripts.js
@@ -1,30 +1,31 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const hamburger = document.querySelector(".hamburger");
-  const mobileMenu = document.querySelector(".mobile-menu");
+//I removed the event listener that wrapped these functions because it didn't always trigger.
 
-  hamburger.addEventListener("click", () => {
-    mobileMenu.style.display =
-      mobileMenu.style.display === "block" ? "none" : "block";
+const hamburger = document.querySelector(".hamburger");
+const mobileMenu = document.querySelector(".mobile-menu");
+
+hamburger.addEventListener("click", () => {
+  console.log("mobileMenu.style.display", mobileMenu.style.display);
+  mobileMenu.style.display =
+    mobileMenu.style.display === "block" ? "none" : "block";
+});
+
+document.querySelectorAll("#menu-main-nav li").forEach((li) => {
+  let timeoutId;
+
+  li.addEventListener("mouseover", () => {
+    const subMenu = li.querySelector(".sub-menu");
+    if (subMenu) {
+      clearTimeout(timeoutId);
+      subMenu.classList.add("nav-show");
+    }
   });
 
-  document.querySelectorAll("#menu-main-nav li").forEach((li) => {
-    let timeoutId;
-
-    li.addEventListener("mouseover", () => {
-      const subMenu = li.querySelector(".sub-menu");
-      if (subMenu) {
-        clearTimeout(timeoutId);
-        subMenu.classList.add("nav-show");
-      }
-    });
-
-    li.addEventListener("mouseout", () => {
-      const subMenu = li.querySelector(".sub-menu");
-      if (subMenu) {
-        timeoutId = setTimeout(() => {
-          subMenu.classList.remove("nav-show");
-        }, 500); // Prevent menu from closing too quickly when you move the mouse
-      }
-    });
+  li.addEventListener("mouseout", () => {
+    const subMenu = li.querySelector(".sub-menu");
+    if (subMenu) {
+      timeoutId = setTimeout(() => {
+        subMenu.classList.remove("nav-show");
+      }, 500); // Prevent menu from closing too quickly when you move the mouse
+    }
   });
 });
